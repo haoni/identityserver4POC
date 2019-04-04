a808663e99085dc308f3594a9dfbd7480e75d776
haoni <haoni@outlook.com>
2019-04-04 10:35:37 -0400

    Identityserver4 setup

---
+++ Domain/Entities/Clients.cs
@@ -0,0 +1,21 @@
+
+
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace identityserver4POC.Domain.Entities {
+    public class Clients {
+        public static IEnumerable<Client> Get() {
+                return new List<Client> {
+                    new Client {
+                        ClientId = "oauthClient",
+                        ClientName = "Example Client Credentials Client Application",
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,
+                        ClientSecrets = new List<Secret> {
+                            new Secret("superSecretPassword".Sha256())},                         
+                        AllowedScopes = new List<string> {"customAPI.read"}
+                    }
+                };
+            }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+// using identityserver4POC.Domain.Entities;
+
+namespace identityserver4POC
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddIdentityServer()
+                .AddInMemoryClients(identityserver4POC.Domain.Entities.Clients.Get())
+                .AddInMemoryIdentityResources(identityserver4POC.Domain.Entities.Resources.GetIdentityResources())
+                .AddInMemoryApiResources(identityserver4POC.Domain.Entities.Resources.GetApiResources())
+                .AddTestUsers(identityserver4POC.Domain.Entities.Users.Get())
+                .AddDeveloperSigningCredential();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            app.UseIdentityServer();
+            
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
